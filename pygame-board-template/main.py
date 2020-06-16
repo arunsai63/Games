11c8b8679c4f07d894c79a9ca737d9f00a1fda51
arunsai63 <arunsai63@gmail.com>
2020-06-16 15:19:42 +0530

    updated code & added template

---
+++ pygame-board-template/main.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame import QUIT, KEYDOWN, K_ESCAPE
+
+pygame.init()
+
+SCREEN_SIZE = 720
+
+
+screen = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE])
+bg_img = pygame.transform.scale(pygame.image.load("assets/board.png"), (SCREEN_SIZE, SCREEN_SIZE))
+
+
+
+running = True
+mouse_click = None
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_click = pygame.mouse.get_pos()
+        elif event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            running = False
+    screen.blit(bg_img, (0, 0))
+
+    if mouse_click is not None:
+        mouse_click = None
+    else:
+        pass
+
+    pygame.display.flip()
+
+pygame.quit()
--- sudoku/solver.py
+++ sudoku/solver.py
@@ -1,23 +1,22 @@
 def valid(board):
-    for row in board: #rows
+    for row, col in [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)]: # blocks
+        numbers = [board[row+i][col+j] for i in range(3) for j in range(3) if board[row+i][col+j]]
+        if len(set(numbers)) != len(numbers):
+            return False
+            
+    for row in board: # rows
         numbers = [num for num in row if num]
         if len(set(numbers)) != len(numbers):
             return False
 
-    for col in range(9): #cols
+    for col in range(9): # cols
         numbers = [board[num][col] for num in range(9) if board[num][col]]
         if len(set(numbers)) != len(numbers):
             return False
-    
-    for row in [0,3,6]: #blocks
-        for col in [0,3,6]:
-            numbers = [board[row+i][col+j] for i in range(3) for j in range(3) if board[row+i][col+j]]
-            if len(set(numbers)) != len(numbers):
-                return False
 
     return True
 
-def solve(board):
+def solve(board): # back-tracking algorithm
     for row in range(9):
         for col in range(9):
             if not board[row][col]:
